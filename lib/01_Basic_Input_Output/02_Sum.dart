5dd11aff83f591d074feb915431fb35f7bf28efa
Tushar-9921 <trparmar.3480@gmail.com>
2026-08-26 21:51:56 +0530

    Initial Commit

---
+++ lib/01_Basic_Input_Output/02_Sum.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+
+  try {
+    stdout.write("Enter First Number: ");
+    double num1 = double.parse(stdin.readLineSync()!);
+
+    stdout.write("Enter Second Number: ");
+    double num2 = double.parse(stdin.readLineSync()!);
+
+    print("\nSum = ${num1 + num2}");
+
+  } on FormatException {
+    print("Please Enter Valid Numbers only.");
+  } catch (e) {
+    print("Other Unexpected Error: $e");
+  }
+}
